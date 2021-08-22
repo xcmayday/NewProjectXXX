1023f413f4f116d6cbc79de1cc7d45348d2cf757
652498718@qq.com <652498718@qq.com>
2021-08-22 23:37:31 +0800

    ccc

--- Plugins/IntelligentCrowd/Source/IntelligentCrowd/Private/Character/IntelligentCrowdCharacterBase.cpp
+++ Plugins/IntelligentCrowd/Source/IntelligentCrowd/Private/Character/IntelligentCrowdCharacterBase.cpp
@@ -11,7 +11,7 @@ AIntelligentCrowdCharacterBase::AIntelligentCrowdCharacterBase()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	CurrentHealth = MaxHealth;
-
+	bIsDead = false;
 }
 
 int32 AIntelligentCrowdCharacterBase::GetTeamID() const
